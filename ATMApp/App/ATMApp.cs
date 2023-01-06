74bdba0867eb8d89c1099740c7b52e8a53efbd73
SaraHadou <s.hadou.user@gmail.com>
2023-01-06 16:07:09 +0200

    first commit

---
+++ ATMApp/App/ATMApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ATMApp
+{
+    class ATMApp
+    {
+        static void Main(string[] args)
+        {
+            // Clear the console
+            Console.Clear();
+            // Set the title of the console
+            Console.Title = "My ATM APP";
+            // Set the text color to white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Set the Welcoming Message
+            Console.WriteLine("\n\n--------------Welcome to Our ATM App--------------\n\n");
+            // Prompt the user to insert his card
+            Console.WriteLine("Please Insert your Card");
+            Console.WriteLine("Note: Actual ATM will accept only physical cards, " +
+                "read the card number and validate it.");
+
+
+            Console.WriteLine("\n\nPlease Enter to continue...\n");
+            Console.ReadLine();
+        }
+    }
+}
